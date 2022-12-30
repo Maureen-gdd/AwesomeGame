1aeb191f1a5cd7d481ae98eb57506b5463d66fb6
Maureen Grandidier <maureen.grandidier@gmail.com>
2022-12-30 11:03:38 +0100

    Initial commit

---
+++ build/CMakeFiles/Cpp_SDL_Program.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/smooshiie/Documents/AwesomeGame/src/SDL2_gfxPrimitives.c" "CMakeFiles/Cpp_SDL_Program.dir/src/SDL2_gfxPrimitives.c.o" "gcc" "CMakeFiles/Cpp_SDL_Program.dir/src/SDL2_gfxPrimitives.c.o.d"
+  "/home/smooshiie/Documents/AwesomeGame/src/application_ui.cpp" "CMakeFiles/Cpp_SDL_Program.dir/src/application_ui.cpp.o" "gcc" "CMakeFiles/Cpp_SDL_Program.dir/src/application_ui.cpp.o.d"
+  "/home/smooshiie/Documents/AwesomeGame/src/main.cpp" "CMakeFiles/Cpp_SDL_Program.dir/src/main.cpp.o" "gcc" "CMakeFiles/Cpp_SDL_Program.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Cpp_SDL_Program.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin/Cpp_SDL_Program"
+  "../bin/Cpp_SDL_Program.pdb"
+  "CMakeFiles/Cpp_SDL_Program.dir/src/SDL2_gfxPrimitives.c.o"
+  "CMakeFiles/Cpp_SDL_Program.dir/src/SDL2_gfxPrimitives.c.o.d"
+  "CMakeFiles/Cpp_SDL_Program.dir/src/application_ui.cpp.o"
+  "CMakeFiles/Cpp_SDL_Program.dir/src/application_ui.cpp.o.d"
+  "CMakeFiles/Cpp_SDL_Program.dir/src/main.cpp.o"
+  "CMakeFiles/Cpp_SDL_Program.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Cpp_SDL_Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
